48233cea760e59828d66161c5529dd4e192805aa
Liam <u7100338@anu.edu.au>
2022-05-13 01:09:28 +1000

    time added

--- simulation.py
+++ simulation.py
@@ -1,6 +1,14 @@
 import numpy as np
-import sympy as sp
+import random
+#import sympy as sp
+#from matplotlib import pyplot as plt
 
-from matplotlib import pyplot as plt
+t0 = 0
 
-print("Hello World")
+tf = 720
+
+dt = 1
+
+time = np.arange(t0, tf, dt, dtype='float')
+
+print(time)
